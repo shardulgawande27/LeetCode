ac337e8f9816fb59356fd38e2bdd00aeefe5fbe5
shardul gawande <85838542+shardulgawande27@users.noreply.github.com>
2022-06-22 17:28:25 +0530

    Time: 355 ms (21.44%), Space: 43.5 MB (5.11%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -12,20 +12,20 @@ class Solution:
     
         
         # Solution 2 easy to understand
-        stack = []
-        for c in s:
-            stack.append(c)
+#         stack = []
+#         for c in s:
+#             stack.append(c)
             
-        i = 0
-        while stack:
-            s[i] = stack.pop()
-            i += 1
+#         i = 0
+#         while stack:
+#             s[i] = stack.pop()
+#             i += 1
 
     
         # solution 3
         
-#         def reverse(l,r):
-#             if l < r:
-#                 s[l], s[r]  = s[r] , s[l]
-#                 reverse(l + 1 , r - 1)
-#         reverse(0, len(s) - 1)
+        def reverse(l,r):
+            if l < r:
+                s[l], s[r]  = s[r] , s[l]
+                reverse(l + 1 , r - 1)
+        reverse(0, len(s) - 1)
